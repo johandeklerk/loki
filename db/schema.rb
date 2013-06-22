c04a304b3220756c525dd495a303e777085cfbfb
johandk <j10f9996>
2013-06-22 19:45:10 +0200

    Add Artist model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130622173650) do
+
+  create_table "artists", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
