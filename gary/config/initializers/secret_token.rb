dc369803f09a8731447e653e6703a673609d0014
Johan De Klerk <johandk@voxtelecom.co.za>
2013-07-07 22:15:56 +0200

    Adding gary, the frontend app

---
+++ gary/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gary::Application.config.secret_key_base = '4c563a8a4c706ebe4cb8e14f6c9da854d323fb5c60842b4cceff4d51df650d135faf341df9bb3b04164c94d79b9a80bb92dc5c194d04ef3282f00e4c5165770d'
---
+++ gary/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gary::Application.config.session_store :cookie_store, key: '_gary_session'
